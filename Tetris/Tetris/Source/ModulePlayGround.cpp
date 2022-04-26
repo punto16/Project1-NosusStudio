bc0086625004a0a37e4b9618bb755d349313c823
Crujialex <crujialex.555551@gmail.com>
2022-04-26 22:21:00 +0200

    Victory condition completed!

--- Tetris/Tetris/Source/ModulePlayGround.cpp
+++ Tetris/Tetris/Source/ModulePlayGround.cpp
@@ -99,7 +99,7 @@ Update_Status ModulePlayGround::PostUpdate()
 	//victory condition
 	if (App->player->lines == App->modules[currentModule]->linesofLevelGetter())
 	{
-
+		App->fade->FadeToBlack(this, (Module*)App->sceneIntro, 90);
 	}
 
 
