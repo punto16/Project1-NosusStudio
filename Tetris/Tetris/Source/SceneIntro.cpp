b512fc601cac7cb898f88107488313c1aaa98937
kikofp02 <kikofp02@gmail.com>
2022-04-26 21:26:56 +0200

    Scene Intro implementation

--- Tetris/Tetris/Source/SceneIntro.cpp
+++ Tetris/Tetris/Source/SceneIntro.cpp
@@ -25,7 +25,8 @@ bool SceneIntro::Start()
 	bool ret = true;
 
 	bgTexture = App->textures->Load("Assets/Sprites/sprite_title.png");
-	//App->audio->PlayMusic("Assets/Music/introTitle.ogg", 1.0f);
+	introTexture = App->textures->Load("Assets/Sprites/PreTitle.png");
+	App->audio->PlayMusic("", 1.0f);
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
@@ -35,13 +36,11 @@ bool SceneIntro::Start()
 
 Update_Status SceneIntro::Update()
 {
-	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
+	if ((!play_intro) && (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN))
 	{
 		App->fade->FadeToBlack(this, (Module*)App->sceneLevel_1, 90);
 	}
-
-
-	else if (App->input->keys[SDL_SCANCODE_ESCAPE] == Key_State::KEY_DOWN)
+	else if ((!play_intro)&&(App->input->keys[SDL_SCANCODE_ESCAPE] == Key_State::KEY_DOWN))
 	{
 		return Update_Status::UPDATE_STOP;
 	}
@@ -53,7 +52,21 @@ Update_Status SceneIntro::Update()
 Update_Status SceneIntro::PostUpdate()
 {
 	// Draw everything --------------------------------------
-	App->render->Blit(bgTexture, 0, 0, NULL);
+	uint tex_w, tex_h;
+	App->textures->GetTextureSize(introTexture, tex_w, tex_h);
+
+	if ((App->render->camera.y / SCREEN_SIZE) == tex_h) {
+		play_intro = false;
+	}
+
+	if (play_intro) {
+		App->render->Blit(introTexture, 0, 0, NULL);
+		App->render->camera.y ++;
+	}
+	else {
+		App->render->camera.y = 0;
+		App->render->Blit(bgTexture, 0, 0, NULL);
+	}
 
 	return Update_Status::UPDATE_CONTINUE;
 }
--- Tetris/Tetris/Source/SceneIntro.h
+++ Tetris/Tetris/Source/SceneIntro.h
@@ -32,6 +32,9 @@ public:
 public:
 	// The scene sprite sheet loaded into an SDL_Texture
 	SDL_Texture* bgTexture = nullptr;
+	SDL_Texture* introTexture = nullptr;
+
+	bool play_intro = true;
 };
 
 #endif
